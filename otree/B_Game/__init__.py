73799dcb120eae50dc1ade85eb0b3fc186642577
Hauke Roggenkamp <haukeroggenkamp@eduroamverw-130-82-25-43.unisg.ch>
2022-06-24 23:42:06 +0200

    initiate MVP

---
+++ otree/B_Game/__init__.py
@@ -0,0 +1,71 @@
+from otree.api import *
+
+
+doc = """
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'B_Game'
+    PLAYERS_PER_GROUP = 2
+    NUM_ROUNDS = 5
+
+    ENDOWMENT = cu(10)
+    ALLOCATOR_ROLE = "Allocator"
+    RECIPIENT_ROLE = "Recipient"
+
+    RECIPIENT_TEMPLATE = "A_Intro/C_Recipient.html"
+    ALLOCATOR_TEMPLATE = "A_Intro/C_Allocator.html"
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    voice_interface = models.BooleanField(doc="treatment variable describing whether the allocator communicates his/her decision orally.")
+
+
+
+class Player(BasePlayer):
+    share = models.IntegerField(doc="the share the dictator allocates to the recipient.",
+                                blank=True)
+
+# FUNCTIONS
+def creating_session(subsession):
+    import itertools
+    voice_interface = itertools.cycle([True, False])
+    for group in subsession.get_groups():
+        group.voice_interface = next(voice_interface)
+
+
+# PAGES
+class A_Decision(Page):
+    form_model = "player"
+    form_fields = ["share"]
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(
+            redirect = "",
+        )
+
+
+
+class ResultsWaitPage(WaitPage):
+    pass
+
+
+class Results(Page):
+    @staticmethod
+    def js_vars(player):
+        dic_share = (1 - player.group.get_player_by_role(C.ALLOCATOR_ROLE).share / 10) * 100
+        return dict(
+            dictators=dic_share,
+            role=player.role
+        )
+    pass
+
+
+page_sequence = [A_Decision, ResultsWaitPage, Results]
--- otree/_static/global/js/pie_chart.js
+++ otree/_static/global/js/pie_chart.js
@@ -1,49 +1,115 @@
-console.log("pie chart & live updates ready!")
+console.log("pie chart ready!")
 
 
 // vars from oTree
-let compound = js_vars.compound;
-
+let dictators = js_vars.dictators;
+let role = js_vars.role;
 
 // initiate variables
-var green = 34;
-if (compound) {
-  green = 67;
-}
-var red = 100 - green;
+var green;
+var red;
 var primary = "#0275d8";
+var pie;
 
-// change content on input
-document.getElementById("id_matching_probability").addEventListener('input', () => {
-    green = parseInt(document.getElementById("id_matching_probability").value) || 0;
+// set values
+
+if(role=="Recipient"){
+    red = dictators;
+    green = 100 -  red;
+}else{
+    green = dictators;
     red = 100 - green;
+}
 
-    if(green<=0){
-        document.getElementById("submitCheckLabel").innerHTML = `<small>I would never choose the bet.</small>`;
-    } else if (green > 100) {
-        document.getElementById("submitCheckLabel").innerHTML = `<small>I would always choose the bet.</small>`;
-    } else {
-        document.getElementById("submitCheckLabel").innerHTML = `<small>For ${green - 1} or less green balls, I would choose the bet.</small>`;
+// set colors
+const pattern = "M 0 0 L 10 10 M 9 -1 L 11 1 M -1 9 L 1 11"; //"M 3 0 L 3 10 M 8 0 L 8 10"; // more patterns here: https://www.highcharts.com/docs/chart-design-and-style/pattern-fills
+const yourColor = "#36D6B0";
+const yourLabel = {
+    backgroundColor: "rgba(255, 255, 255, 0.33)",
+    style: {
+        fontWeight: "normal",
+        textOutline: 0,
+    },
+};
+
+const othersColor = {
+    pattern: {
+        path: pattern,
+        color: "#FF0B5F",
+        width: 10,
+        height: 10,
+        patternTransform: "scale(2)"
     }
-
-    if(template!="instructions"){
-        document.getElementById("submitButton").removeAttribute("disabled");
+};
+const othersLabel = {
+    style: {
+        color: "#000000"
     }
-    document.getElementById("collapseSubmit").setAttribute("aria-expanded", "true");
-    document.getElementById("collapseSubmit").className = "";
+}
 
-    pie.series[0].update({
+
+// viz
+Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie',
+        exporting: {
+                enabled: false
+              },
+    },
+
+    title: {
+                text: ""
+              },
+
+    tooltip: {
+        formatter: function() {
+                  if (this.point.color == yourColor) {
+                    return "Your share";
+                  } else {
+                    return "The other participant's share"
+                  }
+                }
+    },
+    accessibility: {
+        point: {
+            valueSuffix: 'points'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage} %',
+                distance: 15,
+                style: {
+                      fontWeight: "normal"
+                    }
+            }
+        }
+    },
+    series: [{
+        name: 'Allocation',
+        colorByPoint: true,
         data: [{
+            name: "The Other's Share",
             y: red,
-          },
-          {
+            sliced: true,
+            selected: true,
+            color: othersColor
+        }, {
+            name: 'Your Share',
             y: green,
-          }
-        ]
-    });
+            color: yourColor
+        }]
+    }]
 });
 
-var pie;
+/*
 
 // define pie chart
 $(function () {
@@ -116,4 +182,4 @@ $(function () {
             });
          });
 	});
-});
+});*/
